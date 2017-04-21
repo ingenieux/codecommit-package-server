f20413878b969906b3ba1bf1918ef9707bc63000
Aldrin Leal <aldrin@leal.eng.br>
2017-04-21 11:52:15 -0500

    First Version

---
+++ README.md
@@ -0,0 +1,95 @@
+# codecommit-package-server
+
+A Golang Package Server for CodeCommit Repositories
+
+## Installation
+
+```
+$ go get github.com/ingenieux/codecommit-package-server 
+```
+
+This will download and install it, next:
+
+  * Setup your systemd scripts such as:
+
+```
+$ cat /etc/systemd/system/codecommit-package-server.service 
+[Unit]
+Description=CodeCommit Import Server
+After=network.target
+
+[Service]
+User=ubuntu
+Group=ubuntu
+ExecStart=/home/ubuntu/go/bin/codecommit-package-server
+# Fix the path above
+ 
+[Install]
+WantedBy=multi-user.target
+```
+  * Setup nginx:
+
+```
+$ cat /etc/nginx/sites-enabled/codecommit.ingenieux.io
+server {
+  listen 80;
+  listen [::]:80;
+  server_name codecommit.ingenieux.io;
+
+  access_log /var/log/nginx/nginx.codecommit.ingenieux.io.access.log;
+  error_log /var/log/nginx/nginx.codecommit.ingenieux.io.error.log;
+
+  location /.well-known/ {
+    root /var/www/letsencrypt;
+  }
+
+  location / {
+    return 301 https://$host$request_uri;
+  }
+}
+
+# Append the lines below once TLS keys were installed. Remember to change hosts
+
+server {
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
+
+    include snippets/ssl-params.conf;
+
+    server_name codecommit.ingenieux.io;
+
+    access_log /var/log/nginx/nginx.codecommit.ingenieux.io.access.log;
+    error_log /var/log/nginx/nginx.codecommit.ingenieux.io.error.log;
+
+    ssl_certificate /etc/letsencrypt/live/codecommit.ingenieux.io/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/codecommit.ingenieux.io/privkey.pem;
+
+    location / {
+        proxy_set_header X-Forwarded-For $remote_addr;
+        proxy_set_header Host            $http_host;
+        proxy_pass http://localhost:3001/;
+    }
+}
+```
+
+  * Setup SSL with [Certbot](https://github.com/certbot/certbot/)
+
+## Usage
+
+Replace codecommit.ingenieux.io with your package server host. In the example below, myrepo is a :
+
+```
+$ go get -d -v codecommit.ingenieux.io/repo/myrepo
+```
+
+# What about other AWS Regions?
+
+Err, nice question. Currently this public server only supports us-east-1
+
+# What about SSH?
+
+You can add ?protocol=ssh into your URLS. 
+
+Be wary that you must also change that in all your source code. 
+
+When running a custom install, simply set "defaultProto" to "ssh" and it should work.
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/go-zoo/bone"
+	"github.com/gorilla/handlers"
+	"net/http"
+	"os"
+	"text/template"
+	"time"
+)
+
+type RepoInfo struct {
+	RepoId     string
+	RepoUrl    string
+	SourcePath string
+	Protocol   string
+}
+
+var pageTemplate = template.Must(template.New("").Parse(`
+<html>
+  <head>
+    <meta name="go-import" content="{{.RepoId}} git {{.RepoUrl}}">
+    <title>Package Redirect for {{.RepoId}}</title>
+  </head>
+  <body>
+    <p>go get {{.RepoId}}</p>
+    <p>Source: <a href="{{.SourcePath}}">{{.SourcePath}}</a></p>
+    <h2>Setup Instructions</h2>
+    <ul>
+      <li>See <a href="https://alestic.com/2015/11/aws-codecommit-iam-role/">this guide</a></li>
+      <li>Or <a href="http://docs.aws.amazon.com/codecommit/latest/userguide/setting-up.html">read the AWS Docs</a></li>
+      <li><b>Ubuntu 14.04 Users</b>: <a href="https://askubuntu.com/questions/186847/error-gnutls-handshake-failed-when-connecting-to-https-servers">Beware</a></li>
+    </ul>
+  </body>
+</html>
+`))
+
+var hostname string = "codecommit.ingenieux.io"
+var region string = "us-east-1"
+var listenAddr string = "127.0.0.1:3001"
+var defaultProto string = "https"
+
+func main() {
+	flag.StringVar(&hostname, "hostname", hostname, "hostname to use")
+	flag.StringVar(&region, "region", region, "region to use")
+	flag.StringVar(&listenAddr, "listenAddr", listenAddr, "Address to Listen")
+	flag.StringVar(&defaultProto, "defaultProto", defaultProto, "Default Protocol (ssh / https)")
+
+	flag.Parse()
+
+	router := bone.New()
+
+	router.Get("/repo/:id", http.HandlerFunc(RepoHandler))
+	router.Get("/repo/:id/*", http.HandlerFunc(RepoHandler))
+
+	httpServer := &http.Server{
+		Handler:      handlers.LoggingHandler(os.Stderr, router),
+		Addr:         listenAddr,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Info("Starting")
+
+	log.Fatal(httpServer.ListenAndServe())
+}
+
+func RepoHandler(response http.ResponseWriter, request *http.Request) {
+	// https://git-codecommit.us-east-1.amazonaws.com/v1/repos/cowsurfing ||
+	// ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/cowsurfing
+
+	repoId := bone.GetValue(request, "id")
+
+	sourcePath := fmt.Sprintf("https://console.aws.amazon.com/codecommit/home?region=%s#/repository/%s/browse/", region, repoId)
+
+	if region != "us-east-1" {
+		sourcePath = fmt.Sprintf("https://console-%s.aws.amazon.com/codecommit/home?region=%s#/repository/%s/browse/", region, region, repoId)
+	}
+
+	protocol := defaultProto
+
+	if protocolParameter := request.URL.Query().Get("protocol"); "" != protocolParameter {
+		protocol = protocolParameter
+	}
+
+	repoInfo := RepoInfo{
+		RepoId:     fmt.Sprintf("%s/repo/%s", hostname, repoId),
+		RepoUrl:    fmt.Sprintf("%s://git-codecommit.%s.amazonaws.com/v1/repos/%s", protocol, region, repoId),
+		SourcePath: sourcePath,
+		Protocol:   protocol,
+	}
+
+	response.WriteHeader(200)
+	response.Header().Set("Content-Type", "text/html; charset=utf-8")
+	pageTemplate.Execute(response, repoInfo)
+}
